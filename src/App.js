6a1fdf545b7c915437550dc73ab0a6b0599d2d99
kesavan <arunprakash7871@gmail.com>
2023-04-03 06:13:49 -0400

    added in all repositary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './assets/App.css';
+let URL = 'https://course-api.com/react-tabs-project';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    let [company, setcompany] = useState([]);
+    let [isloading, setisloading]= useState(true);
+    let [value, setvalue] = useState(0);
+    // let [value, setvalue] = useState(0);
+    const fetchcompany = async()=>{
+        const responce = await fetch(URL);
+        const data = await responce.json();
+        console.log(data);
+        setcompany(data);
+        setisloading(false);
+    };
+    useEffect(()=>{
+        fetchcompany()
+    }, []);
+    if(isloading){
+        return(
+            <h1>...Loading</h1>
+        )
+        
+    }
+    const {title, duties, dates} = company[value];
+    console.log(title);
+    console.log(company);
+    return (
+        <div>
+             {/* {console.log(value)} */}
+            <h1>Experience</h1>
+            {company.map((val, index)=>{
+                // {console.log(val);}
+                return (
+                <>
+                <button className='job-btn' onClick={()=>{setvalue(index)}}>{val.company}</button>
+
+                </>
+                )
+                // {console.log(val);}
+            })}
+            <div>{dates}</div>
+            <div>{title}</div>
+            <p>{duties}</p>
+            </div>
+    )
 }
-
 export default App;
